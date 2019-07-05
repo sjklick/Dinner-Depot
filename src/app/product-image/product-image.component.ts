0a216570770a437539cc802a32820b8945e3b3d0
Steven Joshua Klickermann <sjklickermann@gmail.com>
2019-07-04 23:57:48 -0400

    Make product-image component actually fetch image

--- src/app/product-image/product-image.component.ts
+++ src/app/product-image/product-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-image',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-image.component.scss']
 })
 export class ProductImageComponent implements OnInit {
-  imgSrc: string;
+  @Input() imgSrc: string;
 
   constructor() { }
 
